c472116989631262d2f7b0aabc1653a5a1de1658
leeyoungjik1 <eog91@naver.com>
2024-06-12 11:45:55 +0900

    데이터베이스 연동 테스트2

--- server/api/index.js
+++ server/api/index.js
@@ -16,7 +16,10 @@ const corsOptions = {
 
 console.log("몽고아틀라스 주소: ", process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('데이터베이스 연결 성공'))
+.then(() => {
+    console.log('데이터베이스 연결 성공')
+    console.log('테스트')
+})
 .catch(e => console.log(`데이터베이스 연결 실패: ${e}`))
 
 app.use(cors(corsOptions))
