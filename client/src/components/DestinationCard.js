af4cd8b5f24df4a4adf253002ce6c408b5643a98
leeyoungjik1 <eog91@naver.com>
2024-06-12 14:51:13 +0900

    시간 테스트

--- client/src/components/DestinationCard.js
+++ client/src/components/DestinationCard.js
@@ -32,7 +32,7 @@ function DestinationCard({title, address, category, timeOfStart, timeOfEnd, desc
         }
     }, [placeId])
     console.log(timeOfStart)
-    console.log(moment(timeOfStart).format())
+    console.log(momentTimezone(timeOfStart).tz('Asia/Seoul').format())
     return (
         <div className={styles.destinationCardContainer}>
             <div className={styles.isDone}>{isDone ? "완료" : "예정"}</div>
